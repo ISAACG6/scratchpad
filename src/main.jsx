3f12a9e0487518cc0dc37725140a3c0210e4ab20
Isaac Gurney <isaacg6@uw.edu>
2026-02-03 13:13:43 -0800

    sets up firebase

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,24 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyADliOr6GSoZ3PPIP4VxQAwl-Lb3dRWLbg",
+  authDomain: "scratchpad-25306.firebaseapp.com",
+  projectId: "scratchpad-25306",
+  storageBucket: "scratchpad-25306.firebasestorage.app",
+  messagingSenderId: "86704062748",
+  appId: "1:86704062748:web:c21696e8f7e3a674f9bad9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
---
+++ src/nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Nav() {
+  return (
+    <nav>
+      <h1>My Application</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
