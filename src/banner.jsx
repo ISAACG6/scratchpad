651fa3eb388468e30d936e249d94ffa35ba54fb7
Isaac Gurney <isaacg6@uw.edu>
2026-02-03 19:50:38 -0800

    fixes name error

--- src/banner.jsx
+++ src/banner.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function banner() {
   return (
     <div className="banner body p-3">
-      <h1><em>SC4TCHP4D</em></h1>
+      <h1><em>SCR4TCHP4D</em></h1>
     </div>
   );
 }
